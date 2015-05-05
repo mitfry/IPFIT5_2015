b0928b1b7ee221220e6c674d1b177ae8d8f4cb6f
mitfry <mitchellhoutkamp@msn.com>
2015-05-05 11:54:29 +0200

    HelloWorld
    Test en bevestiging dat alles werkt bij Mitchell.

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+L = ["a","b","c","d","e","f"]
+print ["5e element"] + [L[4]]
+print ["Laatste elemt"] + [L[-1]]
+print ["Eerste 4 elementen"] + [L[:4]]
+print ["Element 1, 3 en 5"] + [L[0:5:2]]
+print ["De lijst omgekeerd"] + [L[::-1]]
+print "Bij Mitchell werkt alles"
