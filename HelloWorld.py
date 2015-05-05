356a4cc58cf2c73bbae6a7b744e825a5dd186a6e
mitfry <mitchellhoutkamp@msn.com>
2015-05-05 12:14:43 +0200

    Comments aangepast
    "<<< HEAD >>>>" comments uit de code verwijderd.

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,12 +5,6 @@ print ["Eerste 4 elementen"] + [L[:4]]
 print ["Element 1, 3 en 5"] + [L[0:5:2]]
 print ["De lijst omgekeerd"] + [L[::-1]]
 print "Bij Mitchell werkt alles"
-<<<<<<< HEAD
 print "Bij Tim werkt ook alles!"
-=======
-<<<<<<< HEAD
 print "Bij Roland"
-=======
 print "Bij Hugo werkt alles ook!"
->>>>>>> origin/master
->>>>>>> origin/master
