532a6b5459f295b4b7cf73393c1b7cec2ca61df4
mitfry <mitchellhoutkamp@msn.com>
2015-05-05 12:20:48 +0200

    Comments aangepast.
    HEAD en ORIGIN/MASTER comments verwijderd. Test.

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,22 +5,9 @@ print ["Eerste 4 elementen"] + [L[:4]]
 print ["Element 1, 3 en 5"] + [L[0:5:2]]
 print ["De lijst omgekeerd"] + [L[::-1]]
 print "Bij Mitchell werkt alles"
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 print "Bij Roland (2.0)"
-=======
->>>>>>> origin/master
 print "Bij Tim werkt ook alles!"
 print "Bij Roland"
-<<<<<<< HEAD
-=======
->>>>>>> origin/master
-=======
->>>>>>> origin/master
-<<<<<<< HEAD
 print "Bij Hugo werkt alles ook!"
-=======
 print "Bij Andre werkt dit half"
->>>>>>> e3a61997ec6215e7cb9a8dae70a06a3daba3fa85
+
