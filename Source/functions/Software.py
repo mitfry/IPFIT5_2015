ddd25b5e55079fdc38b894195318e255e072ce6a
mitfry <mitchellhoutkamp@msn.com>
2015-06-10 23:38:37 +0200

    Software cleanup
    Software bestand opgeruimd en beter ingedeeld.

--- Source/functions/Software.py
+++ Source/functions/Software.py
@@ -6,8 +6,6 @@ import pythoncom
 
 # Custom
 # Add folder "modules" to the locations to import from
-from threading import Thread
-
 sys.path.append(sys.path[0] + "/../modules")
 
 import wmi
@@ -34,7 +32,7 @@ class WorkerThread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        print 'Gathering information...\n'
+        print 'Gathering information... (0/3)\n'
         pythoncom.CoInitialize()
         w = wmi.WMI(".")
 
@@ -43,20 +41,20 @@ class WorkerThread(threading.Thread):
             if process.Caption is not None:
                 # print "ID: ", process.ProcessId, " Name: ", process.Name
                 list_processes.append([process.ProcessID, process.Name])
-
+        print 'Process list complete! (1/3)\n'
         # Software installed with any windows installer
         # List is incomplete imo
         for software in w.Win32_Product():
             if software.Caption is not None:
                 # print "Name: ", software.Caption.replace(u"\u2122", ''), "on version ", software.Version
                 list_software.append([software.Caption.replace(u"\u2122", ''), software.Version])
-
+        print 'Software list complete! (2/3)\n'
         # Generating a list of services on the target pc
         for service in w.Win32_Service():
             if service.Caption is not None:
                 # print "ID: ", service.ProcessId, " Name: ", service.Caption, " Service state:", service.State
                 list_services.append([service.ProcessId, service.Caption, service.State])
-        print 'Finished gathering information!\nSoftware Tab ready for use :)'
+        print 'Finished gathering information! (3/3)\nSoftware Tab ready for use :)'
 
 
 WorkerThread().start()
